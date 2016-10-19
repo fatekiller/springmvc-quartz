f971dbb20d0db23469d7c22339ea41001f1275aa
fatekiller <liuchenfei.lcf@foxmail.com>
2016-10-19 17:28:01 +0800

    'deleteout'

---
+++ quartz-test/src/com/qt/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.qt.dao;
+
+/**
+ * Created by liuchenfei on 2016/10/19.
+ */
+public class UserDao {
+}
---
+++ quartz-test/src/com/qt/entity/User.java
@@ -0,0 +1,7 @@
+package com.qt.entity;
+
+/**
+ * Created by liuchenfei on 2016/10/19.
+ */
+public class User {
+}
