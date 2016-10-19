c3ecfb44c18b446deffb5d7981f6d63c06c95361
fatekiller <liuchenfei.lcf@foxmail.com>
2016-10-19 15:11:00 +0800

    'addschedule'

---
+++ quartz-test/src/com/qt/service/task/ScheduleJob.java
@@ -0,0 +1,13 @@
+package com.qt.service.task;
+
+import java.util.Date;
+
+/**
+ * Created by liuchenfei on 2016/10/19.
+ */
+public class ScheduleJob {
+    public void work()
+    {
+        System.out.println("working");
+    }
+}
